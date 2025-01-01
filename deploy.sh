6d2ffd886cc502838bb0cf7ad7b6e260229b9d5a
Rajeevan <rajeevdreams25@gmail.com>
2025-01-01 13:53:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,14 +21,22 @@ fi
 if ! command -v pip3 &> /dev/null; then
     echo "Pip3 is not installed. Please install Pip3 and try again."
     echo "Installing Python3 and pip..."
-    python3 -m ensurepip
-    pip3 install --no-cache --upgrade pip setuptools
 
-
+    # Check if the "OS" folder already exists
+    if [ -d "OS" ]; then
+        echo "'OS' directory already exists. Skipping virtual environment creation."
+    else
+        echo "Creating a virtual environment in the 'OS' folder..."
+        python3 -m venv OS
+        cd OS
+    fi
+    
     exit 1
 fi
 
 
+source ./bin/activate
+cd ..
 echo "Installing packages"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
