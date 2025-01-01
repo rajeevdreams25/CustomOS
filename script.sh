d179c9b91933919c0156b4eaea9f69853b44d9cf
Rajeevan <rajeevdreams25@gmail.com>
2025-01-01 20:24:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,28 +33,21 @@ fi
 echo "Script executed successfully."
 echo "---------------------------------------------------------"
 
+if [ -d "OS" ]; then
+    echo "'OS' directory already exists. Skipping virtual environment creation."
+else
+    echo "Creating a virtual environment in the 'OS' folder..."
+    python3 -m venv OS
+fi
 
-if ! command -v pip3 &> /dev/null; then
-    echo "Pip3 is not installed. Please install Pip3 and try again."
-    echo "Installing Python3 and pip..."
+# Now change into the "OS" directory, if exists
+cd OS || { echo "Failed to change directory to OS."; exit 1; }
 
-    # Check if the "OS" folder already exists
-    if [ -d "OS" ]; then
-        echo "'OS' directory already exists. Skipping virtual environment creation."
-    else
-        echo "Creating a virtual environment in the 'OS' folder..."
-        python3 -m venv OS
-    fi
-    
-    # Now change into the "OS" directory, if exists
-    cd OS || { echo "Failed to change directory to OS."; exit 1; }
-
-    # Optionally, activate the virtual environment if it's a new creation
-    if [ ! -z "$(ls -A)" ]; then
-        echo "Activating the virtual environment..."
-        source bin/activate
-        cd ..
-    fi
+# Optionally, activate the virtual environment if it's a new creation
+if [ ! -z "$(ls -A)" ]; then
+    echo "Activating the virtual environment..."
+    source bin/activate
+    cd ..
 fi
 
 echo "******************************************************"
