b749a57b7f0772ede83971203fed56505d4d50fd
Rajeevan <rajeevdreams25@gmail.com>
2025-01-01 13:42:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ set -e
 
 
 echo "Updating package lists..."
-
-REPO_URL="https://github.com/rajeevdreams25/CustomOS.git"  
+ 
 PYTHON_SCRIPT="code/main.py"                        
 
 
@@ -17,18 +16,6 @@ fi
 if ! command -v python3 &> /dev/null; then
     echo "Python3 is not installed. Please install Python3 and try again."
     exit 1
-fi
-
-
-REPO_NAME=$(basename -s .git "$REPO_URL") 
-echo "Cloning the repository..."
-if [ -d "$REPO_NAME" ]; then
-    echo "Repository '$REPO_NAME' already exists. Pulling latest changes..."
-    cd "$REPO_NAME"
-    git pull
-else
-    git clone "$REPO_URL"
-    cd "$REPO_NAME"
 fi
 
 if ! command -v pip3 &> /dev/null; then
